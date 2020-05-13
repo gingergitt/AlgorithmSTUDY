1d1e514bc1f6d23d2717aff90ffb5404403dcc36
gingergitt <mro8822@gmail.com>
2020-05-13 23:49:56 +0900

    0513-algo update

---
+++ Codeup100/codeup8.java
@@ -0,0 +1,16 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class codeup8 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String word = "";
+
+		word = sc.next();
+
+		System.out.println(word);
+	}
+}
